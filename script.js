951074271e2ec238d9d96d130124f2ab8ea8fa5f
rohan ghimire <ghimirerohan0@gmail.com>
2021-07-01 16:15:11 +0545

    js file

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ navbar = document.querySelector('.navbar')
 rightnav = document.querySelector('.rightnav')
 navlist =  document.querySelector('.nav-list')
 
-burger.addEventListener('click',()=>{
+burger.addEventListener('click',()=>{                 {/*used js to toggle navigation bar in resopnsive view only*/}
        rightnav.classList.toggle('rightnav-resp');
        navlist.classList.toggle('ul-resp');
        navbar.classList.toggle('navbar-resp');
