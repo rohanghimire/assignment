0de49910ef62d3ecac4ebe1e586f6739729c7545
rohan ghimire <ghimirerohan0@gmail.com>
2021-07-02 20:29:23 +0545

    js file

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 
 burger = document.querySelector('.burger')
 navbar = document.querySelector('.navbar')
-rightnav = document.querySelector('.rightnav')
+rightnav = document.querySelector('.rightnav')   /*citation= (CodingNepal,2019)*/
 navlist =  document.querySelector('.nav-list')
 
-burger.addEventListener('click',()=>{                 {/*used js to toggle navigation bar in resopnsive view only*/}
+burger.addEventListener('click',()=>{                 /*used js to toggle navigation bar in resopnsive view only*/
        rightnav.classList.toggle('rightnav-resp');
        navlist.classList.toggle('ul-resp');
        navbar.classList.toggle('navbar-resp');
